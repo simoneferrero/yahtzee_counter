1a561a1d3346308d816dea0b4e588ef4122d76ad
Simone Ferrero <simone.ferrero@outlook.com>
2017-01-12 17:04:20 +0000

    Set up points

--- app/components/App.js
+++ app/components/App.js
@@ -2,20 +2,20 @@ var React = require('react');
 
 var App = React.createClass({
   getInitialState: function() {
-    // return {
-    //   testPlayer: {
-    //     name: 'testPlayer',
-    //     ones: '',
-    //     twos: '',
-    //     threes: '',
-    //     fours: '',
-    //     fives: '',
-    //     sixes: '',
-    //     firstPartSum: 0
-    //   }
-    // };
     return {
-      players: []
+      players: [
+        //  testPlayer: {
+        //     name: 'testPlayer',
+        //     ones: '',
+        //     twos: '',
+        //     threes: '',
+        //     fours: '',
+        //     fives: '',
+        //     sixes: '',
+        //     firstPartSum: 0
+        //   }
+        // }
+      ]
     };
   },
 
@@ -47,11 +47,24 @@ var App = React.createClass({
               <input id="newPlayer" type="text" name="newPlayer" placeholder="Player name"
                 className="col-xs-5 col-xs-offset-1" />
               <button id="addMe" onClick={this.addNewPlayer}
-                 className="btn btn-primary col-xs-3 col-xs-offset-1" >Add me!</button>
+                 className="btn btn-primary col-xs-3 col-xs-offset-1">Add me!</button>
             </div>
           </div>
         </form>
         <div>{this.state.players.map(function(player) {return player.name + ' ';})}</div>
+      </div>
+    );
+
+    var firstPartPoints = (
+      <div className="container">
+        <div className="Row">
+          {/* add column for point description (1s, 2s, etc.) with size col-xs-2
+            loop over number of players and create 1 column for each
+            if 1 player, col-xs-10
+            if 2 players, col-xs-5
+            if 3/4 players, col-xs-5 col-sm-3
+            if >5 players, col-xs-5 col-sm-3 col-md-2  */}
+        </div>
       </div>
     );
     return jumbotron;
